89a1a5412698cac17a18ddd9d5de216ab1a9899b
aravindkarri <aravindkarri98495@gmail.com>
2021-09-17 23:45:13 +0530

    Added: code to edit the contacts

--- src/com/bridgelabz/AddressBookMain.java
+++ src/com/bridgelabz/AddressBookMain.java
@@ -2,18 +2,50 @@ package com.bridgelabz;
 
 import java.util.Scanner;
 
-public class AddressBookMain
-{
+public class AddressBookMain {
+
     public static void main(String[] args) {
 
         Scanner scanner = new Scanner(System.in);
-        System.out.println("Enter How many numbers You want to Add ::");
-        int size = scanner.nextInt();
+
         AddressBookService contact = new AddressBookService();
 
-        for (int i = 0; i < size; i++) {
-            contact.addContact();
-            contact.diaplayContacts();
+        while (true) {
+            System.out.println("Please choose from below choices .");
+            System.out.println(" 1 Add Contact "
+                    + "\n 2 Edit Contact "
+                    + "\n 3 Display Contact "
+                    + "\n 4 Quit ");
+            int userChoice = scanner.nextInt();
+            switch (userChoice)
+            {
+                case 1:
+                    System.out.println("Enter How many numbers You want to Add ::");
+                    int size = scanner.nextInt();
+                    for (int i = 0; i < size; i++) {
+                        contact.addContact();
+                    }
+                    break;
+                case 2:
+                    System.out.println("Enter the Email");
+                    String email = scanner.next();
+                    boolean updated = contact.editContact(email);
+                    if(updated)
+                    {
+                        System.out.println("Contact Updated.");
+                    }
+                    else
+                    {
+                        System.out.println("Contact not found");
+                    }
+                    break;
+                case 3:
+                    contact.displayContacts();
+                    break;
+                default:
+                    System.out.println("You just Quit");
+                    System.exit(0);
+            }
         }
     }
 }
--- src/com/bridgelabz/AddressBookService.java
+++ src/com/bridgelabz/AddressBookService.java
@@ -2,13 +2,13 @@ package com.bridgelabz;
 
 import java.util.Scanner;
 
-public class AddressBookService {
+public class AddressBookService{
 
     private int index = 0;
-    Contact[] contacts = new Contact[7];
-
-    public Contact addContact() {
-        Scanner scanner = new Scanner(System.in);
+    Contact[] contacts = new Contact[20];
+    Scanner scanner = new Scanner(System.in);
+    public void addContact()
+    {
         System.out.println("Enter First Name");
         String firstName = scanner.next();
 
@@ -29,16 +29,49 @@ public class AddressBookService {
 
         System.out.println("Enter Email");
         String email = scanner.next();
-        Contact contact = new Contact(firstName, lastName, city, state, zip, phoneNumber, email);
-        contacts[index++] = contact;
-        return contact;
+        Contact contact = new Contact( firstName, lastName, city, state, zip, phoneNumber, email);
+        contacts[index++]= contact;
     }
 
-    public void diaplayContacts() {
+    public boolean editContact(String email)
+    {
+        for (int i = 0; i < contacts.length; i++)
+        {
+            if(contacts[i] != null && contacts[i].getEmail().equalsIgnoreCase(email))
+            {
+                System.out.println("Enter First Name");
+                String firstName = scanner.next();
+
+                System.out.println("Enter last Name");
+                String lastName = scanner.next();
+
+                System.out.println("Enter City");
+                String city = scanner.next();
+
+                System.out.println("Enter State");
+                String state = scanner.next();
+
+                System.out.println("Enter zip");
+                String zip = scanner.next();
+
+                contacts[i].setFirstname(firstName);
+                contacts[i].setLastname(lastName);
+                contacts[i].setCity(city);
+                contacts[i].setState(state);
+                contacts[i].setState(zip);
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public void displayContacts() {
         for (int i = 0; i < contacts.length; i++) {
-            if (contacts[i] != null) {
+            if(contacts[i] != null)
+            {
                 System.out.println(contacts[i]);
             }
         }
     }
+
 }
